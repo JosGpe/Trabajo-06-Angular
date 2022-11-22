203e966a05563ede6dfac7513e192b090530b172
JosGpe <100324579+JosGpe@users.noreply.github.com>
2022-11-21 22:36:35 -0600

    Add files via upload

---
+++ app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Nice', points: 50 },
+  { id: 13, name: 'Bombasto' , points: 51 },
+  { id: 14, name: 'Celeritas', points: 52 },
+  { id: 15, name: 'Magneta', points: 53 },
+  { id: 16, name: 'RubberMan', points: 54 },
+  { id: 17, name: 'Dynama', points: 55 },
+  { id: 18, name: 'Dr. IQ', points: 56 },
+  { id: 19, name: 'Magma', points: 57 },
+  { id: 20, name: 'Tornado', points: 58 }
+];
